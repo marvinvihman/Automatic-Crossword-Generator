3bd221c887117ac6ce6c6a701ba0fb252762115e
Marvin Vihman <marvinvihman@hotmail.com>
2022-05-08 23:56:17 +0300

    Added function to print screen.

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -9,3 +9,21 @@ function redirectLeheküljest() {
 function redirectKodu() {
     window.location.pathname = "/"
 }
+
+function printScreen() {
+    const body = document.getElementById("body");
+    const el0 = document.getElementById("print0");
+    const el1 = document.getElementById("print1");
+    const el2 = document.getElementById("print2");
+    body.style.visibility = "hidden";
+    el0.classList.add("print");
+    el1.classList.add("print");
+    el2.classList.add("print");
+
+    window.print();
+
+    body.style.visibility = "";
+    el0.classList.remove("print");
+    el1.classList.remove("print");
+    el2.classList.remove("print");
+}
